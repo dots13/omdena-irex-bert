b49edd6608abe862abe8754dab27e6fd9ed300f4
Anastasiia Marchenko <38503936+dots13@users.noreply.github.com>
2024-03-16 01:57:15 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,16 +14,6 @@ def load_model(f_checkpoint):
 if __name__ == "__main__":
     max_len, bert = 178, "dccuchile/bert-base-spanish-wwm-uncased"
     tokenizer = TokenizerFromPreTrained(max_len, bert)
-    model5 = "last-epoch-model-2024-02-27-15_22_42_6.pth"
-    f_checkpoint = Path(f"models//{model5}")
-    #if not f_checkpoint.exists():
-    #    load_model(f_checkpoint)
-    load_model(f_checkpoint)
-    path_to_model = f_checkpoint
-    domain_num = 12
-    max_len, bert = 178 , 'dccuchile/bert-base-spanish-wwm-uncased'
-    MDFEND_MODEL = MDFEND(bert, domain_num , expert_num=15 , mlp_dims = [2024 ,1012 ,606])
-    MDFEND_MODEL.load_state_dict(torch.load(f=path_to_model , map_location=torch.device('cpu')))
 
     # dataset
     batch_size = 64
@@ -38,6 +28,17 @@ if __name__ == "__main__":
         "<h3 style='text-align: center; color:white;'>BERT: Misinformation Classification</h3>",
         unsafe_allow_html=True,
     )
+
+    model5 = "last-epoch-model-2024-02-27-15_22_42_6.pth"
+    f_checkpoint = Path(f"assets/models//{model5}")
+    if verify_checkpoint(model5, f_checkpoint, "1-4NIx36LmRF2R5T8Eu5Zku_-CGvV07VE"):
+        MODEL_SAVE_PATH = f"assets/models/last-epoch-model-2024-02-27-15_22_42_6.pth"
+        MDFEND_MODEL = MDFEND(
+            bert, domain_num, expert_num=15, mlp_dims=[2024, 1012, 606]
+        )
+        MDFEND_MODEL.load_state_dict(
+            torch.load(f=MODEL_SAVE_PATH, map_location=torch.device("cpu"))
+        )
 
     testing_path = "sample.json"
     user_input = st.text_area("Enter Text to Analyze")
